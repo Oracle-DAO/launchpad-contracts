227036ca8775be65f87b9fdd80b2aa98944b3fe0
crypt3311 <cryptoninja101@gmail.com>
2022-03-22 15:35:48 +0530

    Added scripts to gitignore

--- scripts/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-npx hardhat run ./deployScripts/MockProjectToken.ts --network oasis &&
-npx  hardhat run ./deployScripts/MockStableCoin.ts --network oasis &&
-npx hardhat run ./deployScripts/MockStakedToken.ts --network oasis &&
-npx hardhat run ./deployScripts/CorePad.ts --network oasis &&
-npx hardhat run ./deployScripts/PublicSale.ts --network oasis &&
-npx hardhat run ./deployScripts/initialize.ts --network oasis
